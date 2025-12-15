3d933eb52abb9261f515c980c9fd5b2af3a5f437
Reshma <goreshma42@gmail.com>
2025-12-15 21:07:54 +0530

    Utility

---
+++ eclipse-workspace/GrocceryApplication/src/main/java/utility/PageUtility.java
@@ -0,0 +1,39 @@
+package utility;
+
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.Select;
+
+public class PageUtility {
+	public void selectDropdownWithValue(WebElement element, String value) {
+
+		Select object = new Select(element);
+		object.selectByValue(value);
+
+	}
+
+	public void clickOnElement(WebElement element) {
+	    element.click();
+	}
+	
+	public void enterText(WebElement element, String text) {
+	    element.clear();
+	    element.sendKeys(text);
+	}
+	
+	public void pressEnterKey(WebElement element) {
+	    element.sendKeys(Keys.ENTER);
+	}
+	
+	public void clearTextField(WebElement element) {
+	    element.clear();
+	}
+	
+	public void mouseHoverOnElement(WebDriver driver, WebElement element) {
+	    Actions actions = new Actions(driver);
+	    actions.moveToElement(element).perform();
+	}
+
+}
